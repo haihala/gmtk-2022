b9d7e22a20f2f3d0cb18487ff442ec2927dbab70
Eero Häihälä <eero.haihala@gmail.com>
2022-07-15 22:32:37 +0300

    Encounter structure

---
+++ patclib/src/encounter/encounters.rs
@@ -0,0 +1,5 @@
+use super::Encounter;
+
+pub fn get_random_encounter() -> Encounter {
+    todo!()
+}
--- patclib/src/encounter/mod.rs
+++ patclib/src/encounter/mod.rs
@@ -1,7 +1,33 @@
 use bevy::prelude::*;
 
+use crate::battle::Battle;
+
+mod encounters;
+pub use encounters::get_random_encounter;
+
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
-pub struct Encounter;
+pub enum EncounterOptionOutcome {
+    Battle(Battle),
+    // Blocked by not having a player struct
+    // Purchase {cost: i32, reward: todo!()} ,
+    // Reward(),
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct EncounterDecision {
+    prompt: String,
+    options: Vec<(String, EncounterOptionOutcome)>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub enum EncounterPhase {
+    Battle(Battle),
+    Line(String),
+    Decision(EncounterDecision),
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct Encounter(Vec<EncounterPhase>);
 
 pub struct EncounterPlugin;
 
