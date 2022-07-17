47a0536ab98dcf39c14ec70abd203a2bdb12c49a
Eero Häihälä <eero.haihala@gmail.com>
2022-07-17 09:22:41 +0300

    Divide encounters up into separate functions

--- patclib/src/encounter/encounters.rs
+++ patclib/src/encounter/encounters.rs
@@ -14,116 +14,130 @@ fn encounters() -> Vec<Encounter> {
     let wolves = Battle;
 
     vec![
-        Encounter::from_phases(vec![
-            EncounterPhase::Line("This is a test encounter"),
-            EncounterPhase::Decision(EncounterDecision {
-                prompt: "Would you like to fight?",
-                options: vec![
-                    (
-                        "Hell yeah!",
-                        Box::new(EncounterPhase::Battle(wolves.clone())),
-                    ),
-                    (
-                        "Would rather not",
-                        Box::new(EncounterPhase::Gain(
-                            "You avoid the pointless fight, have a money",
-                            PlayerResources {
-                                money: 1,
-                                ..default()
-                            },
-                        )),
-                    ),
-                ],
-            }),
-            EncounterPhase::Line("This encounter is over"),
-        ]),
-        Encounter::from_phases(vec![
-            EncounterPhase::Line("Wolves attack!"),
-            EncounterPhase::Battle(wolves.clone()),
-        ]),
-        Encounter::from_phases(vec![
-            EncounterPhase::Line("A herd of electric sheep are barreling your way"),
-            EncounterPhase::Decision(EncounterDecision {
-                prompt: "What do you do?",
-                options: vec![
-                    (
-                        "Attempt to count them",
-                        Box::new(EncounterPhase::Lose(
-                            "You start feeling drowsy, the sheep trample over you, costing you some time and energy",
-                            PlayerResources { stamina: 20,..default()}
-                        ))
-                    ),
-                    (
-                        "Stand your ground",
-                        Box::new(EncounterPhase::Battle(
-                            Battle
-                        ))
-                    )
-                ],
-
-            }),
-        ]),
-        Encounter::from_phases(vec![
-            EncounterPhase::Line("You see a merchant travelling down the road"),
-            EncounterPhase::Decision(EncounterDecision {
-                prompt: "Would you like to see my wares?",
-                options: vec![
-                    (
-                        "Sure, why not.",
-                        Box::new(EncounterPhase::Decision(EncounterDecision {
-                            prompt: "What do you choose?",
-                            options: vec![
-                                (
-                                    "Ammo!",
-                                    Box::new(EncounterPhase::Trade(
-                                        "You get some ammo.",
-                                        "You don't have enough money.",
-                                        PlayerResources {
-                                            money: 3,
-                                            ..default()
-                                        },
-                                        PlayerResources {
-                                            bullets: 6,
-                                            ..default()
-                                        },
-                                    )),
-                                ),
-                                (
-                                    "Food",
-                                    Box::new(EncounterPhase::Trade(
-                                        "You manage to gain some energy.",
-                                        "You don't have enough money.",
-                                        PlayerResources {
-                                            money: 2,
-                                            ..default()
-                                        },
-                                        PlayerResources {
-                                            stamina: 4,
-                                            ..default()
-                                        },
-                                    )),
-                                ),
-                                (
-                                    "Mystery box",
-                                    Box::new(EncounterPhase::Trade(
-                                        "You gain a mystery box!",
-                                        "You don't have enough money.",
-                                        PlayerResources {
-                                            money: 2,
-                                            ..default()
-                                        },
-                                        PlayerResources { ..default() },
-                                    )),
-                                ),
-                            ],
-                        })),
-                    ),
-                    (
-                        "No thank you.",
-                        Box::new(EncounterPhase::Line("The merchant continues his journey.")),
-                    ),
-                ],
-            }),
-        ]),
+        test_encounter(wolves.clone()),
+        wolf_fight(wolves),
+        electric_sheep(),
+        merchant(),
     ]
+}
+
+fn test_encounter(wolves: Battle) -> Encounter {
+    Encounter::from_phases(vec![
+        EncounterPhase::Line("This is a test encounter"),
+        EncounterPhase::Decision(EncounterDecision {
+            prompt: "Would you like to fight?",
+            options: vec![
+                ("Hell yeah!", Box::new(EncounterPhase::Battle(wolves))),
+                (
+                    "Would rather not",
+                    Box::new(EncounterPhase::Gain(
+                        "You avoid the pointless fight, have a money",
+                        PlayerResources {
+                            money: 1,
+                            ..default()
+                        },
+                    )),
+                ),
+            ],
+        }),
+        EncounterPhase::Line("This encounter is over"),
+    ])
+}
+
+fn wolf_fight(wolves: Battle) -> Encounter {
+    Encounter::from_phases(vec![
+        EncounterPhase::Line("Wolves attack!"),
+        EncounterPhase::Battle(wolves),
+    ])
+}
+
+fn electric_sheep() -> Encounter {
+    Encounter::from_phases(vec![
+        EncounterPhase::Line("A herd of electric sheep are barreling at you"),
+        EncounterPhase::Decision(EncounterDecision {
+            prompt: "What to do in time of crisis?",
+            options: vec![
+                (
+                    "Stand your ground",
+                    Box::new(EncounterPhase::Battle(Battle)),
+                ),
+                (
+                    "Attempt to count them",
+                    Box::new(EncounterPhase::Lose(
+                        "You start feeling drowsy and the herd tramples you",
+                        PlayerResources {
+                            stamina: 20,
+                            ..default()
+                        },
+                    )),
+                ),
+            ],
+        }),
+        EncounterPhase::Line("The semimechanical bovine have been dealt with, but at what cost"),
+    ])
+}
+
+fn merchant() -> Encounter {
+    Encounter::from_phases(vec![
+        EncounterPhase::Line("You see a merchant travelling down the road"),
+        EncounterPhase::Decision(EncounterDecision {
+            prompt: "Would you like to see my wares?",
+            options: vec![
+                (
+                    "Sure, why not.",
+                    Box::new(EncounterPhase::Decision(EncounterDecision {
+                        prompt: "What do you choose?",
+                        options: vec![
+                            (
+                                "Ammo!",
+                                Box::new(EncounterPhase::Trade(
+                                    "You get some ammo.",
+                                    "You don't have enough money.",
+                                    PlayerResources {
+                                        money: 3,
+                                        ..default()
+                                    },
+                                    PlayerResources {
+                                        bullets: 6,
+                                        ..default()
+                                    },
+                                )),
+                            ),
+                            (
+                                "Food",
+                                Box::new(EncounterPhase::Trade(
+                                    "You manage to gain some energy.",
+                                    "You don't have enough money.",
+                                    PlayerResources {
+                                        money: 2,
+                                        ..default()
+                                    },
+                                    PlayerResources {
+                                        stamina: 4,
+                                        ..default()
+                                    },
+                                )),
+                            ),
+                            (
+                                "Mystery box",
+                                Box::new(EncounterPhase::Trade(
+                                    "You gain a mystery box!",
+                                    "You don't have enough money.",
+                                    PlayerResources {
+                                        money: 2,
+                                        ..default()
+                                    },
+                                    PlayerResources { ..default() },
+                                )),
+                            ),
+                        ],
+                    })),
+                ),
+                (
+                    "No thank you.",
+                    Box::new(EncounterPhase::Line("The merchant continues his journey.")),
+                ),
+            ],
+        }),
+    ])
 }
