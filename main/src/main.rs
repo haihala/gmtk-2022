862dfcdf402c24720160e7d9f25ecb8968db5a0e
Eero Häihälä <eero.haihala@gmail.com>
2022-07-17 19:19:05 +0300

    Bundle assets into final build

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,9 +1,12 @@
 use bevy::prelude::*;
+use bevy_embedded_assets::EmbeddedAssetPlugin;
 use patclib::PatcPlugins;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins_with(DefaultPlugins, |group| {
+            group.add_before::<bevy::asset::AssetPlugin, _>(EmbeddedAssetPlugin)
+        })
         .add_plugins(PatcPlugins)
         .run();
 }
